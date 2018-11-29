0119c69099d63bb6177beb8df7b007bbddb4219b
ttercero <ttercero@zam.com>
2018-11-29 12:43:17 -0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Goodby World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
